535b01402e44213acab9f21d4c94d66be4d37805
EdgarAbaddon <161787338+EdgarAbaddon@users.noreply.github.com>
2024-05-30 16:01:08 -0600

    Delete properties directory

--- properties/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Property
-
-class PropertyForm(forms.ModelForm):
-    class Meta:
-        model = Property
-        fields = ['description', 'street', 'colony', 'city', 'postal_code', 'external_number', 'price', 'type', 'wide', 'long', 'ranking', 'image', 'owner']
